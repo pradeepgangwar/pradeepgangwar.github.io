49baf7216833ab56dd6eceb413a8915e431564c7
pradeepgangwar <pradeepgangwar39@gmail.com>
2017-06-06 17:56:34 +0530

    Added new Projects/ Removed unnecessary files

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
    
+    $('.scrollspy').scrollSpy();
     var options = [
-    {selector: '#scrol', offset: 500, callback: 'Materialize.fadeInImage("#scrol")' }
-  ];
-  Materialize.scrollFire(options);
+      {selector: '#nav_f', offset: 500, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } },
+      {selector: '#index-banner', offset: 500, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } },
+      {selector: '#intro', offset: 50, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } },
+      {selector: '#work', offset: 200, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } }
+    ];
+    Materialize.scrollFire(options);
+
+    $('.modal').modal();
+
   });
+
